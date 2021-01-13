9c1c0d8e8b83d7b596796c993381dc7de205aedf
gnuhack <carlospajuelo_@hotmail.com>
2021-01-13 13:22:38 +0100

    Reorganizado un poco con comentarios.

--- emacs.el
+++ emacs.el
@@ -1,3 +1,13 @@
+;;---------------- APARIENCIA ----------------
+
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
+
+;;---------------- KEYBINDINGS ---------------
+
 (key-chord-define-global "jj" 'ace-window)
+
+;;---------------- ELFEED ----------------
+;;TODO Aprender a instalar automáticamente paquetes.
+;;TODO Quitar los warnings de la izquierda ">>"
+;;(require 'elfeed)
