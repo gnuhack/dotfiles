bf92a81e68f4550e1d811f10c6c51466e83b44ac
gnuhack <carlospajuelo_@hotmail.com>
2021-01-13 22:09:48 +0100

    Definidas más variables

--- emacs.el
+++ emacs.el
@@ -16,7 +16,7 @@
 (prelude-require-packages '(elfeed));;Añadir después de elfeed el resto de paquetes a autoinstalar.
 (global-set-key (kbd "C-x w") 'elfeed)
 (declare-function key-chord-define-global"")
-(eval-when-compile (defvar elfeed-feeds));;Eliminación de una queja del compilador
+(eval-when-compile (defvar elfeed-feeds) (defvar calendar-latitude) (defvar calendar-longitude) (defvar org-capture-templates));;Eliminación de una queja del compilador
 (setq elfeed-feeds
  (quote
   ("https://videos.lukesmith.xyz/feeds/videos.xml?sort=-publishedAt&filter=local" ("https://www.youtube.com/feeds/videos.xml?channel_id=UCaifrB5IrvGNPJmPeVOcqBA" Kruggsmash) ("https://www.bay12games.com/dwarves/dev_now.rss" Dwarf Fortress) "https://www.youtube.com/feeds/videos.xml?channel_id=UCD6VugMZKRhSyzWEWA9W2fg" "https://www.youtube.com/feeds/videos.xml?channel_id=UC2eYFnH61tmytImy1mTYvhA" "https://www.youtube.com/channel/UCaifrB5IrvGNPJmPeVOcqBA" "https://www.youtube.com/user/SsethTzeentach" "http://planet.emacs-es.org/rss20.xml" "https://planet.emacslife.com/atom.xml")))
