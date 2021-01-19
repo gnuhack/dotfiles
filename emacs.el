8133be1abd70ab302fe31e6f120b2dbd7bd722f0
gnuhack <carlospajuelo_@hotmail.com>
2021-01-19 21:57:51 +0100

    Arreglados warnings
    Reorganizado el código para que se evalúen las variables libres antes de que den los warings. Añadida la sección de miscelánea al principio del todo y metido dentro las definiciones de las variables.

--- emacs.el
+++ emacs.el
@@ -7,6 +7,17 @@
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 
+;;---------------- MISCELÁNEA ----------------
+;;Arreglamiento de warnings
+
+(declare-function key-chord-define-global"")
+(eval-when-compile (defvar elfeed-feeds) (defvar calendar-latitude) (defvar calendar-longitude) (defvar org-capture-templates) (defvar org-file-apps));;Eliminación de una queja del compilador
+
+;;Puesta de sol
+(setq calendar-latitude 37.344444)
+(setq calendar-longitude -6.065278)
+
+
 ;;---------------- KEYBINDINGS ---------------
 
 (key-chord-define-global "jj" 'ace-window)
@@ -18,14 +29,12 @@
 (declare-function prelude-require-packages"")
 (prelude-require-packages '(elfeed));;Añadir después de elfeed el resto de paquetes a autoinstalar.
 (global-set-key (kbd "C-x w") 'elfeed)
-(declare-function key-chord-define-global"")
-(eval-when-compile (defvar elfeed-feeds) (defvar calendar-latitude) (defvar calendar-longitude) (defvar org-capture-templates) (defvar org-file-apps));;Eliminación de una queja del compilador
 (setq elfeed-feeds
  (quote
   ("https://videos.lukesmith.xyz/feeds/videos.xml?sort=-publishedAt&filter=local" ("https://www.youtube.com/feeds/videos.xml?channel_id=UCaifrB5IrvGNPJmPeVOcqBA" Kruggsmash) ("https://www.bay12games.com/dwarves/dev_now.rss" Dwarf Fortress) "https://www.youtube.com/feeds/videos.xml?channel_id=UCD6VugMZKRhSyzWEWA9W2fg" "https://www.youtube.com/feeds/videos.xml?channel_id=UC2eYFnH61tmytImy1mTYvhA" "https://www.youtube.com/channel/UCaifrB5IrvGNPJmPeVOcqBA" "https://www.youtube.com/user/SsethTzeentach" "http://planet.emacs-es.org/rss20.xml" "https://planet.emacslife.com/atom.xml")))
 
 ;;---------------- ORG ----------------
-;;TODO No se cargan las plantillas
+;;DONE No se cargan las plantillas
 (global-set-key "\C-cc" 'org-capture)
 (setq org-capture-templates
   (quote
@@ -44,11 +53,6 @@
     ("k" "Cita" entry
      (file+datetree+prompt "~/org/uni.org")
      "* %?"))))
-
-;;---------------- MIACELÁNEA ----------------
-;;Puesta de sol
-  (setq calendar-latitude 37.344444)
-  (setq calendar-longitude -6.065278)
 
 (provide 'emacs)
 ;;; emacs.el ends here
