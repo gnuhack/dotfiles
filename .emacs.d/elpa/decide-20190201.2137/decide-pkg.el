6c82964aae3d7c58a1e33505d62f833b1017755a
gnuhack <63854950+gnuhack@users.noreply.github.com>
2021-03-04 00:07:26 +0100

    Delete .emacs.d directory

--- .emacs.d/elpa/decide-20190201.2137/decide-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/carlos/.emacs.d/elpa/decide-20190201.2137/decide.el  -*- no-byte-compile: t -*-
-(define-package "decide" "20190201.2137" "rolling dice and other random things" 'nil :commit "4bfcc826dd5b1c30caec455d8baa4f363159eac6" :authors '(("Pelle Nilsson" . "perni@lysator.liu.se")) :maintainer '("Pelle Nilsson" . "perni@lysator.liu.se"))
