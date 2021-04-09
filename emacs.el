111368fbad039936157449ce10c91837df8f61e1
gnuhack <carlospajuelo_@hotmail.com>
2021-04-09 11:04:51 +0200

    Añadidas notificaciones

--- emacs.el
+++ emacs.el
@@ -42,6 +42,45 @@
   (defvar smtpmail-smtp-server)
   (defvar smtpmail-smtp-service)
   (defvar gnus-select-method));;Eliminación de una queja del compilador
+;;---------------- NOTIFICACIONES ----------------
+(require 'appt)
+(appt-activate t)
+
+(setq appt-message-warning-time 5) ; Show notification 5 minutes before event
+(setq appt-display-interval appt-message-warning-time) ; Disable multiple reminders
+(setq appt-display-mode-line nil)
+
+; Use appointment data from org-mode
+(defun my-org-agenda-to-appt ()
+  (interactive)
+  (setq appt-time-msg-list nil)
+  (org-agenda-to-appt))
+
+; Update alarms when...
+; (1) ... Starting Emacs
+(my-org-agenda-to-appt)
+
+; (2) ... Everyday at 12:05am (useful in case you keep Emacs always on)
+(run-at-time "12:05am" (* 24 3600) 'my-org-agenda-to-appt)
+
+; (3) ... When TODO.txt is saved
+(add-hook 'after-save-hook
+          '(lambda ()
+             (if (string= (buffer-file-name) (concat (getenv "HOME") "/ideas/TODO.txt"))
+                 (my-org-agenda-to-appt))))
+
+; Display appointments as a window manager notification
+(setq appt-disp-window-function 'my-appt-display)
+(setq appt-delete-window-function (lambda () t))
+
+(setq my-appt-notification-app (concat (getenv "HOME") "/bin/appt-notification"))
+
+(defun my-appt-display (min-to-app new-time msg)
+  (if (atom min-to-app)
+    (start-process "my-appt-notification-app" nil my-appt-notification-app min-to-app msg)
+  (dolist (i (number-sequence 0 (1- (length min-to-app))))
+    (start-process "my-appt-notification-app" nil my-appt-notification-app (nth i min-to-app) (nth i msg)))))
+
 ;;---------------- APARIENCIA ----------------
 ;;; Code:
 (menu-bar-mode 0)
