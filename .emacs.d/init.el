e08b6f6d1decae15256c54e89a638c712a5e3b11
gnuhack <carlospajuelo_@hotmail.com>
2021-09-15 17:13:44 +0200

    Org-static.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,7 +29,7 @@
  '(org-agenda-files
    '("~/org/uni.org" "~/Nextcloud/Documents/horario.txt" "~/Nextcloud/Talk/orgt430/gtd.org"))
  '(package-selected-packages
-   '(dashboard org-roam zenburn-theme hledger-mode scratch sudoku notmuch ebdb dictionary emojify telega projectile org-bullets company which-key helm htmlize pdf-tools magit emms elfeed 2048-game))
+   '(org-static-blog dashboard org-roam zenburn-theme hledger-mode scratch sudoku notmuch ebdb dictionary emojify telega projectile org-bullets company which-key helm htmlize pdf-tools magit emms elfeed 2048-game))
  '(telega-mode-line-string-format
    '("   "
      (:eval
