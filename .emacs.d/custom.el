d18e7192ee8f0d496c479ff4f1b6f0c15e8f3b1d
Carlos Pajuelo Rojo <carlos@x230>
2022-12-11 02:26:55 +0100

    Añadido fichero org

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -32,7 +32,7 @@
  '(eww-search-prefix "http://www.searx.be/?q=")
  '(hledger-ratios-essential-expense-accounts "expenses:casa expenses:comida expenses:mama")
  '(hledger-ratios-liquid-asset-accounts "assets:cuenta assets:efectivo")
- '(org-agenda-files '("/home/carlos/org/uni.org"))
+ '(org-agenda-files '("/home/carlos/org/uni.org" "/home/carlos/org/gtd.org"))
  '(package-selected-packages
    '(org-alert tzc notmuch-indicator telega gnuplot gnuplot-mode multi-vterm vterm jabber ytdious dired-subtree hackernews dired-hacks-utils tmr poker minesweeper mines disk-usage company-ebdb rainbow-delimiters osm smart-mode-line popper org-books marginalia crossword gif-screencast org-tree-slide helm-notmuch org-roam-ui helm-org-rifle rainbow-mode decide engine-mode org-pomodoro modus-themes exec-path-from-shell org-static-blog dashboard org-roam zenburn-theme hledger-mode scratch sudoku notmuch ebdb dictionary emojify projectile org-bullets company which-key helm htmlize pdf-tools magit emms elfeed 2048-game))
  '(telega-mode-line-string-format
