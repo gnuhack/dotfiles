ba7de2cdaa556d791df00c9fb5738ce1c1662b4a
Carlos Pajuelo Rojo <carlos@x230>
2023-01-12 22:46:43 +0100

    Añadido modo menor para jugar al tetris

---
+++ tetritecla.el
@@ -0,0 +1,43 @@
+(make-variable-buffer-local
+  (defvar tetritecla-basic-mode nil
+    "Alterna tetritecla-basic-mode."))
+
+(defvar tetritecla-basic-mode-map (make-sparse-keymap)
+  "El keymap para tetritecla-basic-mode")
+
+;; Definición de las teclas para jugar.
+(define-key tetritecla-basic-mode-map (kbd "j")
+  (lambda ()
+    (interactive)
+    (tetris-move-left)))
+    
+(define-key tetritecla-basic-mode-map (kbd "k")
+  (lambda ()
+    (interactive)
+    (tetris-rotate-prev)))     
+
+(define-key tetritecla-basic-mode-map (kbd "h")
+  (lambda ()
+    (interactive)
+    (tetris-move-down)))
+
+(define-key tetritecla-basic-mode-map (kbd "l")
+  (lambda ()
+    (interactive)
+    (tetris-move-right)))
+
+(add-to-list 'minor-mode-alist '(tetritecla-basic-mode " tetritecla"))
+(add-to-list 'minor-mode-map-alist (cons 'tetritecla-basic-mode tetritecla-basic-mode-map))
+
+(defun tetritecla-basic-mode (&optional ARG)
+  (interactive (list 'toggle))
+  (setq tetritecla-basic-mode
+        (if (eq ARG 'toggle)
+            (not tetritecla-basic-mode)
+          (> ARG 0)))
+
+  
+  ;; Aviso de activación de modo
+  (if tetritecla-basic-mode
+      (message "tetritecla-basic-mode activado")
+    (message "tetritecla-basic-mode desactivado")))
