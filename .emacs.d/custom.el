b70719784725135751d9fc1d9535602a250ce637
carlos <carlos@x230>
2023-09-09 13:04:04 +0200

    Feed de Planet Emacs eliminado

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -20,7 +20,6 @@
      ("https://www.youtube.com/feeds/videos.xml?channel_id=UCaifrB5IrvGNPJmPeVOcqBA" Kruggsmash Video)
      ("https://www.bay12games.com/dwarves/dev_now.rss" Dwarf Fortress Video)
      ("https://www.youtube.com/feeds/videos.xml?channel_id=UCD6VugMZKRhSyzWEWA9W2fg" Video)
-     ("http://planet.emacs-es.org/rss20.xml" Emacs Planet)
      ("http://karl-voit.at/feeds/lazyblorg-all.atom_1.0.links-and-content.xml" Blog Voit)
      ("https://planet.emacslife.com/atom.xml" Emacs)
      ("https://protesilaos.com/master.xml" Prot Blog)
