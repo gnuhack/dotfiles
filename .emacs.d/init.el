96db3a275f348be89e400e10c4ab6106518eb338
Carlos Pajuelo <carlos@pajuelo.com>
2026-01-09 10:39:58 +0100

    Mejorado init.el para que falle menos al instalar paquetes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,14 @@
 ;;  -*- lexical-binding: t; -*-
 ;;Configuración mínima para debuggear: https://stackoverflow.com/questions/19336489/initializing-emacs-with-org-babel-debugger-entered-lisp-error-void-function
 
+;;REPOSITORIOS
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3") ; Solución a un bug de Emacs 27 o por ahí.
+
+(setq package-check-signature nil)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")
+			 ))
+
 ;; INICIALIZACIÓN DE PAQUETES Y TANGLE PARA ORG-MODE
 (package-initialize)
 (require 'ob-tangle)
@@ -9,27 +17,18 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
-;;REPOSITORIOS
-  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3") ; Solución a un bug de Emacs 27 o por ahí.
-
-
-  (setq package-archives '(("elpa" . "http://tromey.com/elpa/")
-			   ("gnu" . "http://elpa.gnu.org/packages/")
-			   ("melpa" . "https://melpa.org/packages/")
-			   ))
-
 ;; PAQUETES
 
-  (unless package-archive-contents
-    (package-refresh-contents))
+(unless package-archive-contents
+  (package-refresh-contents))
 
 (package-install-selected-packages)
 
 ;; COMPILACIÓN NATIVA
 
-  (setq comp-deferred-compilation t) ;;Compilación nativa
-  (setq native-comp-async-report-warnings-errors 'silent) ; emacs28 with native compilation
-  ;;https://www.masteringemacs.org/article/speed-up-emacs-libjansson-native-elisp-compilation
+(setq comp-deferred-compilation t) ;;Compilación nativa
+(setq native-comp-async-report-warnings-errors 'silent) ; emacs28 with native compilation
+;;https://www.masteringemacs.org/article/speed-up-emacs-libjansson-native-elisp-compilation
 
 ;; MIS ARCHIVOS
 
